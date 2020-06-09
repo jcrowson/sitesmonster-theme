235da66ffd9c1bfd36b965e9dbd2734b2639b115
James <TJVvmnsNzH6{Ptbin6PDdDGi>
2020-06-09 09:36:44 +0100

    Add now page

---
+++ now.php
@@ -0,0 +1,30 @@
+<?php /* Template Name: Now */ ?>
+
+<?php get_header(); ?>
+
+<main role="main" class="pt-5">
+  <section class="jumbotron d-flex justify-content-between align-items-center">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-md-8 offset-md-1">
+          <h1>I am...</h1>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="container">
+    <div class="row">
+      <div class="col-12 col-md-8 offset-md-1">
+        <ul class="fa-ul mb-5">
+          <li><span class="fa-li"><i class="fas fa-chevron-right fa-xs text-muted"></i></span>Embarking on a journey to take a fresh blog <a href="https://sitesmonster.com/journey/from-0-to-100000-monthly-pageviews">from 0 to 100,000 monthly pageviews</a></li>
+          <li><span class="fa-li"><i class="fas fa-chevron-right fa-xs text-muted"></i></span>Writing a minimum of one article a day</li>
+          <li><span class="fa-li"><i class="fas fa-chevron-right fa-xs text-muted"></i></span>Talking to people about content marketing & SEO</li>
+          <li><span class="fa-li"><i class="fas fa-chevron-right fa-xs text-muted"></i></span>Writing React code (a JavaScript library)</li>
+          <li><span class="fa-li"><i class="fas fa-chevron-right fa-xs text-muted"></i></span>Walking, a lot</li>
+        </ul>
+      </div>
+    </div>
+  </section>
+</main>
+
+<?php get_footer(); ?>
