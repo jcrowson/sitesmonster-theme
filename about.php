f9ec1506e96ef27949b2c076e4062b0f23b25a3c
James <TJVvmnsNzH6{Ptbin6PDdDGi>
2020-06-09 09:26:23 +0100

    Add about page

--- about.php
+++ about.php
@@ -1,4 +1,4 @@
-<?php /* Template Name: Contact */ ?>
+<?php /* Template Name: About */ ?>
 
 <?php get_header(); ?>
 
@@ -9,7 +9,11 @@
         <div class="col-12 col-md-8 offset-md-1">
           <h1>Hello, and Welcome to SitesMonster!</h1>
           <p>
-            I'm James King, and I own a network of websites and blogs that serve pages to over 120,000 people every month.
+            I'm James King, and I own a network of sites and blogs that receive over 120,000 visitors every month.
+          </p>
+          <p>
+            I'm embarking on a journey to take a fresh blog from 0 to 100,000 monthly pageviews, and I'm inviting you to join me. I'll be sharing everything I do, every step of the way. So if you're interested in
+            creating your own blog, and want to learn how to build a successful blog, this experiment of mine will likely be of huge interest to you.
           </p>
         </div>
       </div>
@@ -20,7 +24,7 @@
       <div class="col-12 col-md-8 offset-md-1">
         <h2 class="text-muted pt-2">Get In Touch</h2>
         <p>
-          If you'd like to get in touch, you can reach out to me at <a href="mailto:hello@sitesmonster.com">hello@sitesmonster.com</a>.
+          If you'd like to get in touch, you can reach out to me at <a href="mailto:hello@upmostly.com">hello@upmostly.com</a>.
         </p>
         <p>
           A few facts about me:
@@ -28,7 +32,7 @@
         <ul class="fa-ul mb-5">
           <li><span class="fa-li"><i class="fas fa-chevron-right fa-xs text-muted"></i></span>British 🇬🇧</li>
           <li><span class="fa-li"><i class="fas fa-chevron-right fa-xs text-muted"></i></span>Now living in the United States 🇺🇸</li>
-          <li><span class="fa-li"><i class="fas fa-chevron-right fa-xs text-muted"></i></span>Have a successful network of sites and blogs which 120,000 people read every month.</li>
+          <li><span class="fa-li"><i class="fas fa-chevron-right fa-xs text-muted"></i></span>Have a successful network of sites and blogs which over 120,000 people read every month</li>
           <li><span class="fa-li"><i class="fas fa-chevron-right fa-xs text-muted"></i></span>Drummer 🥁</li>
         </ul>
       </div>
